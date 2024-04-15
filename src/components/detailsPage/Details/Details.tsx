f6a69fc0fd73305b3fd52abed4603be399022fc6
taylorblankenship <taylorblankenship66@gmail.com>
2024-04-14 20:21:13 -0400

    shiny toggle

--- src/components/detailsPage/Details/Details.tsx
+++ src/components/detailsPage/Details/Details.tsx
@@ -1,15 +1,30 @@
 import { usePokemonDetails, usePokemonSpeciesInfo } from "@/hooks/usePokemon";
 import styles from "./Details.module.css";
 import StatsCard from "../StatsCard/StatsCard";
+import { useState } from "react";
 
 const Details = ({ pokemonName }: { pokemonName: string }) => {
   const { pokemonDetails, isLoading: isLoadingDetails } =
     usePokemonDetails(pokemonName);
   const { pokemonSpeciesInfo, isLoading: isLoadingSpeciesInfo } =
     usePokemonSpeciesInfo(pokemonName);
+  const [isShiny, setIsShiny] = useState(false);
   return (
     <div className={styles.detailsContainer}>
       <h2>{pokemonName}</h2>
+      <img
+        src={
+          isShiny
+            ? pokemonDetails?.sprites.other["official-artwork"].front_shiny
+            : pokemonDetails?.sprites.other["official-artwork"].front_default
+        }
+        alt={
+          isShiny
+            ? `${pokemonName} shiny artwork`
+            : `${pokemonName} default artwork`
+        }
+      />
+      <button onClick={() => setIsShiny(!isShiny)}>Is Shiny Toggle</button>
       {isLoadingDetails || isLoadingSpeciesInfo ? (
         <>
           <div>Loading</div>
